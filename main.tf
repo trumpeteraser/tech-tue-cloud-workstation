9e49f9c7fb64ac22eb79d4b920fd9f66a3c74739
Nick Zeller <github@zellfam.net>
2024-04-28 02:53:44 +0000

    added VPC to main.tf

--- main.tf
+++ main.tf
@@ -41,3 +41,51 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"]
 }
+
+resource "aws_vpc" "work-vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = "true" #gives you an internal domain name
+  enable_dns_hostnames = "true" #gives you an internal host name
+  instance_tenancy     = "default"
+
+  tags = {
+    Name = "work-vpc"
+  }
+}
+
+resource "aws_subnet" "work-subnet" {
+  vpc_id                  = aws_vpc.work-vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true" #it makes this a public subnet
+  availability_zone       = "${var.aws_region}a"
+  tags = {
+    Name = "work-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "work-igw" {
+  vpc_id = aws_vpc.work-vpc.id
+  tags = {
+    Name = "work-igw"
+  }
+}
+
+resource "aws_route_table" "work-rtble" {
+  vpc_id = aws_vpc.work-vpc.id
+
+  route {
+    //associated subnet can reach everywhere
+    cidr_block = "0.0.0.0/0"
+    //CRT uses this IGW to reach internet
+    gateway_id = aws_internet_gateway.work-igw.id
+  }
+
+  tags = {
+    Name = "work-rtble"
+  }
+}
+
+resource "aws_route_table_association" "work-rta" {
+  subnet_id      = aws_subnet.work-subnet.id
+  route_table_id = aws_route_table.work-rtble.id
+}
